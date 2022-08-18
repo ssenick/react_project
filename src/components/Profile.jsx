c7c00cec0237a09cc7486c7587acbf47a93bb09b
Ruslan <ssenick@ukr.net>
2022-08-18 14:47:34 +0200

    git test

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,11 +10,13 @@ const Profile = () => {
             </div>
             <div className="profile__about">
                 <div className="profile__avatar">
-                    <img src="https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51401141-stock-illustration-male-avatar-profile-picture-use.jpg" alt=""/>
+                    <img
+                        src="https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51401141-stock-illustration-male-avatar-profile-picture-use.jpg"
+                        alt=""/>
                 </div>
                 <div className="profile__info">
                     <div className="profile__name">Ruslan S.</div>
-                    <div className="profile__date">Date of Birth: 14 january   </div>
+                    <div className="profile__date">Date of Birth: 14 january</div>
                     <div className="profile__city">City: Namestovo</div>
                     <div className="profile__web-site">Web-site: https://greenshok.ssenick.fun</div>
                 </div>
