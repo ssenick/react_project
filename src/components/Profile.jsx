624603dd413cbdf1d3eb2c9283a34e16c1b601dc
Ruslan <ssenick@ukr.net>
2022-08-18 16:49:47 +0200

    test commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -21,7 +21,9 @@ const Profile = () => {
                     <div className={prof.profile__webSite}>Web-site: https://greenshok.ssenick.fun</div>
                 </div>
             </div>
-            <div className="profile__myPosts"></div>
+            <div className="profile__myPosts">
+                sdfsdfdsfds
+            </div>
         </div>
     )
 }
