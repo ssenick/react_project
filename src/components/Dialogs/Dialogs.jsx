773492e72010d0399c597e9f480aa7b17ae9b37b
Ruslan <ssenick@ukr.net>
2022-08-21 12:05:05 +0200

    lessons 21 верстка месседжера

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-   return <div>Masseges</div>;
+   return (
+      <div className={s.dialogs}>
+         <div className={s.dialogsItems}>
+            <div className={s.dialog + ' ' + s.active}>Igor</div>
+            <div className={s.dialog}>Ira</div>
+            <div className={s.dialog}>Alona</div>
+            <div className={s.dialog}>Daniil</div>
+            <div className={s.dialog}>Mama</div>
+            <div className={s.dialog}>Papa</div>
+         </div>
+         <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>How is your name?</div>
+            <div className={s.message}>Dima ))</div>
+         </div>
+      </div>
+   )
 };
 
 export default Dialogs;
