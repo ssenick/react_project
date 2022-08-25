c63c125420630399567f01aef72105c9699a41d6
Ruslan <ssenick@ukr.net>
2022-08-25 14:59:22 +0200

    lessons 24 создание дата и перептсование кода оращаясь к етим датам

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,7 @@ import {NavLink} from "react-router-dom";
 const DialogItem = (props) => {
    let path = "/dialogs/" + props.id;
    return (
-      <NavLink to={path} className={s.dialog + ' ' + s.active}>{props.name}</NavLink>
+      <NavLink to={path} className={s.dialog}>{props.name}</NavLink>
    )
 }
 
@@ -16,11 +16,25 @@ const Message = (props) => {
 }
 
 const Dialogs = () => {
+
+   let dialogsData = [
+      {id: 1, name: 'Igor'},
+      {id: 2, name: 'Ira'},
+      {id: 3, name: 'Alona'},
+      {id: 4, name: 'Igor'},
+      {id: 5, name: 'Daniil'},
+      {id: 6, name: 'Mama'},
+   ]
+   let messagesData = [
+      {id: 1, message: 'hi'},
+      {id: 2, message: 'How is your name?'},
+      {id: 3, message: 'Dima )'},
+   ]
    return (
       <div className={s.dialogs}>
          <div className={s.dialogsItems}>
-            <DialogItem name="Igor" id="1"/>
-            <DialogItem name="Ira" id="2"/>
+            <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+            <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             <DialogItem name="Alona" id="3"/>
             <DialogItem name="Igor" id="4"/>
             <DialogItem name="Daniil" id="5"/>
@@ -29,9 +43,9 @@ const Dialogs = () => {
 
          </div>
          <div className={s.messages}>
-            <Message text="hi"/>
-            <Message text="How is your name?"/>
-            <Message text="Dima )"/>
+            <Message text={messagesData[0].message}/>
+            <Message text={messagesData[1].message}/>
+            <Message text={messagesData[2].message}/>
          </div>
       </div>
    )
