bae3eb979c7b967fa8e4667c72e0a293138fd3af
Ruslan <ssenick@ukr.net>
2022-08-27 13:08:23 +0200

    lessons 31 MyPosts добавил оброботчик onClick + работа с React.createRef()

--- src/components/Content/Profile/MyPosts/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,22 @@ import MyPost from "./Post/MyPost";
 
 
 const MyPosts = (props) => {
-   let postsElement = props.postData.map((p, i) => <MyPost key={i} src={p.src} text={p.message} like={p.like}/>)
+   let newPostElement = React.createRef();
+   let addPost = () => {
+      let text = newPostElement.current.value !== '' ? newPostElement.current.value : "Нет ничего";
+      alert(text)
+   }
+
    return (
       <div className={posts.myPosts}>
          <h3 className={posts.title}>My posts</h3>
-         <textarea name="posts" className={posts.textArea}></textarea>
-         <button className={posts.postsBtn}>OK</button>
+         <textarea ref={newPostElement} name="posts" className={posts.textArea}></textarea>
+         <button onClick={addPost} className={posts.postsBtn}>OK</button>
          <div className={posts.postsItems}>
 
-            {postsElement}
+            {props.postData
+               .map((p, i) => <MyPost key={i} src={p.src} text={p.message} like={p.like}/>)}
+
          </div>
       </div>
    );
