9d7fa3e508866c347e28e0a2028da963e32225c2
Ruslan <ssenick@ukr.net>
2022-08-27 13:20:08 +0200

    lessons 31 Messages onClick + работа с React.createRef()

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -5,6 +5,8 @@ import Message from "./Message/Message";
 
 
 const Dialogs = (props) => {
+   let textarea = React.createRef();
+
    return (
       <div className={s.dialogs}>
          <div className={s.dialogsItems}>
@@ -12,9 +14,17 @@ const Dialogs = (props) => {
                .map((d, i) => <DialogItem key={i} name={d.name} id={d.id}/>)}
             {/*<NavLink to="/dialogs/1" className={s.dialog + ' ' + s.active}>Igor</NavLink>*/}
          </div>
-         <div className={s.messages}>
-            {props.dialogsPage.messagesData
-               .map((m, i) => <Message key={i} text={m.message}/>)}
+         <div className={s.blockMessages}>
+            <div className={s.messages}>
+               {props.dialogsPage.messagesData
+                  .map((m, i) => <Message key={i} text={m.message}/>)}
+            </div>
+            <div className={s.messageAction}>
+               <textarea ref={textarea} className={s.messageTextarea}></textarea>
+               <button onClick={() => alert(textarea.current.value !== '' ? textarea.current.value : 'Ops')}
+                       className={s.messageBtn}>Send
+               </button>
+            </div>
          </div>
       </div>
    )
